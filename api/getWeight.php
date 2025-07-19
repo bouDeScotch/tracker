d4b85cf4a8781f50b55aebfe98adf72f01a8bf1d
bouDeScotch <florian.garcia18@gmail.com>
2025-07-20 01:39:45 +0200

    Refactor getWeight API to fetch weights from JSON file and handle GET requests

--- api/getWeight.php
+++ api/getWeight.php
@@ -2,9 +2,45 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-// TODO: Actually fetch data from the files
+session_name('tracker_session');
+session_start();
 
+// TODO : REMOVE THIS, HERE FOR TESTING !!!
+$_SESSION['email'] = "test@example.com";
+
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+  echo json_encode(["error" => "This API endpoint should be used with a GET request."]);
+  exit();
+}
+
+if (! isset($_SESSION['email'])) {
+  echo json_encode(["error" => "You should be logged in to call this API endpoint"]);
+  exit();
+}
+$email = $_SESSION['email'];
+
+require_once __DIR__ . "/../lib/helpers.php";
+
+$weightsData = loadJSONFile(__DIR__ . "/../data/weights.json");
+if (! isset($weightsData[$email])) {
+  echo json_encode([
+    "amount" => 0,
+    "data" => []
+  ]);
+  exit();
+}
+
+$weights = $weightsData[$email];
+
+if (! isset($_GET['number'])) {
+  $maxNumberOfWeights = count($weights);
+} else {
+  $maxNumberOfWeights = $_GET['number'];
+}
+
+$weightsInRange = array_slice($weights, -$maxNumberOfWeights);
 echo json_encode([
-  'dates' => ['01/07', '02/07', '03/07', '04/07', '05/07', '06/07', '07/07'],
-  'weights' => [72, 71.9, 71.7, 71.8, 71.6, 71.5, 71.3]
+  "amount" => count($weightsInRange),
+  "data" => $weightsInRange
 ]);
+exit(); 
